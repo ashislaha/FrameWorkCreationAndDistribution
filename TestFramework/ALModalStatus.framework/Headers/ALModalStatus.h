fb2aa0a22d6b1eed3246d15bf47174fa9d8054f4
Ashis Laha <ashis.laha@olacabs.com>
2018-03-06 11:43:31 +0530

    small changes on project

---
+++ TestFramework/ALModalStatus.framework/Headers/ALModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  ALModalStatus.h
+//  ALModalStatus
+//
+//  Created by Ashis Laha on 05/03/18.
+//  Copyright © 2018 Ashis Laha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ALModalStatus.
+FOUNDATION_EXPORT double ALModalStatusVersionNumber;
+
+//! Project version string for ALModalStatus.
+FOUNDATION_EXPORT const unsigned char ALModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALModalStatus/PublicHeader.h>
+
+
